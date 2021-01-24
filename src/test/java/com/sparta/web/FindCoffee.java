468bc13bfdb1a7f22225d0cda3da36eeab3b4d6f
SergeyZubets <SergeyZubets@coherentsolutions.com>
2021-01-25 01:31:54 +0300

    Added HW for Web1 - Find Coffee on e-dostavka

---
+++ src/test/java/com/sparta/web/FindCoffee.java
@@ -0,0 +1,20 @@
+package com.sparta.web;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class FindCoffee {
+
+    @Test
+    public void FindCoffeeLavazza() {
+        double pricePerKilo = 64.98;
+        open("https://e-dostavka.by/");
+        $x("//div[@class='main_menu__inner']//child::li[@id='main_menu__search']").click();
+        $x("//input[@id='searchtext']").setValue("Lavazza qualita oro 500").pressEnter();
+        String price = $x("//div[@class='price']").getText().replaceAll("[а-яА-Я]", "").replaceAll(".$", "");
+        Assert.assertEquals(2 * Double.parseDouble(price), pricePerKilo, "Цена за 1кг не совпадает");
+    }
+}
+
