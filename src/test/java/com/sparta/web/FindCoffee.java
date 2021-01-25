2015cd064c3cef7c6670a1c2b5100d12aa9ce478
SergeyZubets <SergeyZubets@coherentsolutions.com>
2021-01-25 14:26:37 +0300

    FindCoffeeLavazza1 and FindCoffeeLavazza2 were updated according to the HW

--- src/test/java/com/sparta/web/FindCoffee.java
+++ src/test/java/com/sparta/web/FindCoffee.java
@@ -13,17 +13,25 @@ public class FindCoffee {
         open("https://e-dostavka.by/");
         $x("//div[@class='main_menu__inner']//child::li[@id='main_menu__search']").click();
         $x("//input[@id='searchtext']").setValue("Lavazza qualita oro 500").pressEnter();
-        String price = $x("//div[@class='price']").getText().replaceAll("[а-яА-Я]", "").replaceAll(".$", "");
-        Assert.assertEquals(2 * Double.parseDouble(price), pricePerKilo, "Цена за 1кг не совпадает");
+        String price = $x("//div[@class='price_byn']/div[@class='price']").getText()
+                .replaceAll("[а-яА-Я]", "")
+                .replaceAll(".$", "");
+        Assert.assertEquals(
+                2 * Double.parseDouble(price),
+                pricePerKilo,
+                "Цена за 1кг не совпадает");
     }
 
     @Test
     public void FindCoffeeLavazza2() {
-        String pricePerHalfKilo = "32р.49к."; // pricePerKilo / 2 = 64р.98к. / 2 = 32р.49к.
+        String coffeeLavazza500 = "«Lavazza» qualita oro, 500";
         open("https://e-dostavka.by/");
         $x("//div[@class='main_menu__inner']//child::li[@id='main_menu__search']").click();
-        $x("//input[@id='searchtext']").setValue("Lavazza qualita oro 500").pressEnter();
-        String price = $x("//div[@class='price']").getText();
-        Assert.assertEquals(price, pricePerHalfKilo, "Цена за 1кг не совпадает");
+        $x("//input[@id='searchtext']").setValue(coffeeLavazza500).pressEnter();
+        $x("//a[contains(text(),'" + coffeeLavazza500 + "')]").click();
+        Assert.assertEquals(
+                $x("//ul[@class='description']/li[last()]/span").getText(),
+                "64.98",
+                "Цена за 1кг не совпадает");
     }
 }
